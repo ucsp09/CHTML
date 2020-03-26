a50f87f759db4c71eaeb987aa82dc0189ba5519e
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-03-26 21:50:58 +0530

    A Sample test File

--- test/test.cpp
+++ test/test.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 int main()
 {
-  return 0;
+	try{
+		html obj;
+		obj.create_file("sample");
+		obj.add_title("Welcome");
+		obj.add_heading("Hello World");
+		string cars[3][2]={{"Car","Price"},{"BMW","100k"},{"Lamborgini","200k"}};
+		obj.add_table(cars);
+		obj.add_paragraph("abcdefghijklmnopqrstuvxyzabcdefghijklmnopqrstuvwxyz");
+		string names[]={"vegeta","goku","gohan","jiren","hit","frieza"};
+		obj.add_list(names,"ol");
+	}
+	catch(const char* msg){
+		cout<<msg;
+	}
+	return 0;
 }
