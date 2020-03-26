c149f5f2127c7fbd331c8527454274234a7f0bd1
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-03-26 20:21:40 +0530

    Added required functions fixes #6,#7,#8

--- lib/chtml.h
+++ lib/chtml.h
@@ -22,4 +22,57 @@ class html{
 					SEEKP;
 					fout<<"<title>"<<title_content<<"</title>"<<"\n</body>\n</html>";
 				}
+	void add_link(string linkurl,string linkname)
+				{
+					CHECK;
+					SEEKP;
+					fout<<"<a href=\""<<linkurl<<"\">"<<linkname<<"</a>"<<"\n</body>\n</html>";
+				}
+				void add_button(string button_name)
+				{
+					CHECK;
+					SEEKP;
+					fout<<"<button>"<<button_name<<"</button>"<<"\n</body>\n</html>";
+				}
+				template<size_t LEN>
+				void add_list(string (&list)[LEN],string type="ul")
+				{
+					CHECK;
+					if(type=="ul"||type=="ol")
+					{
+						SEEKP;
+						fout<<"<"<<type<<">\n";
+						for(int i=0;i<LEN;i++)
+							fout<<"<li>"<<list[i]<<"</li>\n";
+						fout<<"</"<<type<<">"<<"\n</body>\n</html>";
+					}
+					else
+						throw "Please choose list type from one of these{ul,ol}";
+				}
+				void add_image(string imageurl,string alt="user_image",int width=100,int height=100)
+				{
+					CHECK;
+					SEEKP;
+					fout<<"<img src=\""<<imageurl<<"\""<<" "<<"alt=\""<<alt<<"\""<<" "<<"width=\""<<width<<"\""<<" "<<"height=\""<<height<<"\">"<<"\n</body>\n</html>";
+				}
+				template<size_t ROW,size_t COL>
+				void add_table(string (&table)[ROW][COL])
+				{
+					CHECK;
+					SEEKP;
+					fout<<"<table>\n";
+					for(int i=0;i<ROW;i++)
+					{
+						fout<<"<tr>\n";
+						for(int j=0;j<COL;j++)
+						{
+							if(i==0)
+								fout<<"<th>"<<table[i][j]<<"</th>\n";
+							else
+								fout<<"<td>"<<table[i][j]<<"</td>\n";
+						}
+						fout<<"</tr>\n";
+					}
+					fout<<"</table>"<<"\n</body>\n</html>";
+				}
 };
