9de47f4aa09fe2bf54f8abf838b7ccb5089f21e2
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-03-27 17:21:00 +0530

    Add files via upload

---
+++ lib/def.h
@@ -0,0 +1,9 @@
+#define OPEN fout.open(filename)
+#define INIT fout<<"<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n</body>\n</html>"
+#define GOTOB fout.seekp(-16,ios_base::end);
+#define GOTOH fout.seekp(-33,ios_base::end);
+#define CLOSE fout.close()
+#define CHECK if(!fout.is_open())throw "Please create a file first using create_file()"
+#define STAB "border:1px solid black;border-collapse:collapse;"
+#define SPAR "color:black;font:Arial;"
+#define SHED "color:black;font:Arial;"
