34934f288b35ca58b4f77080dc88905309f5fd69
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-04-08 21:10:49 +0530

    Creation of a plain html file with no styling using html class

---
+++ examples/Example1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include"chtml.h"
+using namespace std;
+int main()
+{
+	html obj;
+	obj.add_title("Welcome");
+	obj.add_header("Hello World",2);
+	obj.add_para("abcdefghijklmnopqrstuvwxyz");
+	obj.add_link("#abcd","Click me");
+	string fruits[]={"apple","banana","mango","guava","grapes"};
+	obj.add_list(fruits,"ol");
+	string items[3][4]={{"Name","Qty","Price","Location"},{"Apple","120","11","Hyderabad"},{"Mango","97","20","Mumbai"}};
+	obj.add_table(items);
+	obj.open_tag("div");
+	obj.add_button("Press here");
+	obj.close_tag("div");
+	obj.openln_tag("div");
+	obj.insert_text("this is a form");
+	obj.closeln_tag("div");
+	return 0;
+}
