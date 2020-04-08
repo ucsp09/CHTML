45c50916824c56c35372972e8f70dc3db0337e6f
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-04-08 18:49:12 +0530

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -5,14 +5,14 @@ int main()
 {
 	try{
 		html obj;
-		obj.create_file("sample");
 		obj.add_title("Welcome");
-		obj.add_heading("Hello World");
+		obj.add_header("Hello World");
 		string cars[3][2]={{"Car","Price"},{"BMW","100k"},{"Lamborgini","200k"}};
 		obj.add_table(cars);
 		obj.add_paragraph("abcdefghijklmnopqrstuvxyzabcdefghijklmnopqrstuvwxyz");
 		string names[]={"vegeta","goku","gohan","jiren","hit","frieza"};
 		obj.add_list(names,"ol");
+		obj.dump("sample");
 	}
 	catch(const char* msg){
 		cout<<msg;
