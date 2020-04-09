bb8b86cc1c84cbde07928fc06ec41070cad68d0f
ucsp09 <51616719+ucsp09@users.noreply.github.com>
2020-04-09 19:14:55 +0530

    Update Example6.cpp

--- examples/Example6.cpp
+++ examples/Example6.cpp
@@ -1,8 +1,10 @@
+/*Creation of a html file with embedded styling using html class*/
 #include<iostream>
 #include"chtml.h"
 using namespace std;
 int main()
 {
+	try{
 	html obj;
 	obj.add_title("Welcome");
 	obj.add_header("Hello",2);
@@ -11,6 +13,10 @@ int main()
 	obj.add_style("tag","h2","color:red;font:Arial;");
 	obj.add_style("class","pwd","color:blue;");
 	obj.add_style("id","qwd","color:green;");
-	obj.dump();
+	obj.dump("sample");
+	}
+	catch(const char *msg){
+		cout<<msg;
+	}
 	return 0;
 }
